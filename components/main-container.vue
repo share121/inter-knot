fee37c8c4e333ddf4f5ec320c163e7463319df8e
share121 <2854631158@qq.com>
2024-08-10 22:02:55 +0800

    "更新助手版本信息：1.1.0 -> 1.2.0"

--- components/main-container.vue
+++ components/main-container.vue
@@ -2,7 +2,7 @@
   <main>
     <div class="center" v-if="needUpdata">
       <a href="https://greasyfork.org/zh-CN/scripts/502874" class="link">
-        请更新“绳网小助手”，最新版本为 1.1.0
+        请更新“绳网小助手”，最新版本为 1.2.0
       </a>
     </div>
     <div class="center" v-else-if="needInstall">
