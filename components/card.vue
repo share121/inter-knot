9e90343b7b77687e188c11e97932e460096571e9
TrafficLight6 <3214653283@qq.com>
2024-08-10 20:40:41 +0800

    修改回了随机数的浏览量

--- components/card.vue
+++ components/card.vue
@@ -28,7 +28,7 @@
               d="M1.182 12C2.122 6.88 6.608 3 12 3s9.878 3.88 10.819 9c-.94 5.12-5.427 9-10.819 9s-9.878-3.88-10.818-9M12 17a5 5 0 1 0 0-10a5 5 0 0 0 0 10m0-2a3 3 0 1 1 0-6a3 3 0 0 1 0 6"
             ></path>
           </svg>
-          977
+          {{ viewCounter }}
         </div>
       </section>
       <section class="info-container">
@@ -58,6 +58,7 @@ const isCoverErr = ref(false);
 const isCoverLoaded = ref(false);
 const cover = ref<HTMLImageElement | undefined>();
 const { width, height } = useElementSize(cover);
+const viewCounter=Math.ceil(Math.random()*1000)       //random,data of view in the line 31
 watch(
   () => [width.value, height.value],
   () => {
