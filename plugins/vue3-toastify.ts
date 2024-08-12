07255f3465a463799b20b1569d5d66058e66f265
WestCraneLi <1565024126@qq.com>
2024-08-13 01:54:10 +0800

    feat: 修复vue3-toastify插件主题设置

--- plugins/vue3-toastify.ts
+++ plugins/vue3-toastify.ts
@@ -2,7 +2,7 @@ import Vue3Toastify, { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Vue3Toastify, { theme: "dark" });
+  nuxtApp.vueApp.use(Vue3Toastify, { theme: "dark", limit: 1 });
 
   return {
     provide: { toast },
