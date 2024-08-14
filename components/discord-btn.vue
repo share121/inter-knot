3869feb541d0d325f43ffce50bff74bf2439d2a9
share121 <2854631158@qq.com>
2024-08-14 23:56:22 +0800

    "优化Discord按钮的布局和样式"
    移除不必要的模板字符和属性，简化了discord-btn.vue组件的模板代码。此外，移除了对特定屏幕宽度的样式处理，以优化按钮的布局和响应式设计。

--- components/discord-btn.vue
+++ components/discord-btn.vue
@@ -1,9 +1,5 @@
 <template>
-  <a
-    target="_blank"
-    :href="`https://discord.gg/Cx83Dw9X9E`"
-    title="Discord"
-  >
+  <a target="_blank" :href="`https://discord.gg/Cx83Dw9X9E`" title="Discord">
     <img src="~/assets/svg/discord.svg" alt="" width="62px" />
   </a>
 </template>
@@ -13,12 +9,6 @@ const store = useConfigStore();
 </script>
 
 <style scoped lang="less">
-@media (max-width: 1150px) {
-  a {
-    margin-left: auto;
-  }
-}
-
 @media (max-width: 470px) {
   a {
     display: none;
