3cdd077b0ade05681d57296c2e068a76722f7a40
share121 <share121@users.noreply.github.com>
2024-08-30 05:02:26 +0000

    deploy: 6f5b861ddb673acec3ba44bd9deed20bda6792fe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1368216884"
+    serviceWorkerVersion: "519711278"
   }
 });
