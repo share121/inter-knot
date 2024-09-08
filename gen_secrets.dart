a3ce8a07f33786e37653aee5fa6fec11720b71ae
share121 <2854631158@qq.com>
2024-09-08 12:25:55 +0800

    f

--- gen_secrets.dart
+++ gen_secrets.dart
@@ -1,12 +1,9 @@
 import 'dart:io';
 
 void main() {
-  final pwd = Platform.environment['PWD'];
   final pem = Platform.environment['PEM'];
   final clientId = Platform.environment['CLIENT_ID'];
   final clientSecret = Platform.environment['CLIENT_SECRET'];
   File('lib/secret.dart').writeAsString(
       "const pem = '''$pem'''; const clientId = '$clientId'; const clientSecret = '$clientSecret';");
-  File('android/key.properties').writeAsString(
-      'storePassword=$pwd\nkeyPassword=$pwd\nkeyAlias=upload\nstoreFile=../../upload-keystore.jks');
 }
