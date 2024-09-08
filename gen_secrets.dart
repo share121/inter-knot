de85ff9012bf3716128a2345a213111a68c1d67c
share121 <2854631158@qq.com>
2024-09-08 13:10:10 +0800

    f

--- gen_secrets.dart
+++ gen_secrets.dart
@@ -5,5 +5,5 @@ void main() {
   final clientId = Platform.environment['CLIENT_ID'];
   final clientSecret = Platform.environment['CLIENT_SECRET'];
   File('lib/secret.dart').writeAsString(
-      "const pem = '''$pem'''; const clientId = '$clientId'; const clientSecret = '$clientSecret';");
+      "final pem = String.fromCharCodes(base64Decode('$pem')); final clientId = String.fromCharCodes(base64Decode('$clientId')); final clientSecret = String.fromCharCodes(base64Decode('$clientSecret'));");
 }
